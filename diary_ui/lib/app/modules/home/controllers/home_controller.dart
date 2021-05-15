e9702e313568152c686c1238465a1b6ab345c702
Md. Asaduzzaman Rony <ronyku1829@gmail.com>
2021-05-15 12:22:24 +0600

    Base Project

---
+++ diary_ui/lib/app/modules/home/controllers/home_controller.dart
@@ -0,0 +1,22 @@
+import 'package:get/get.dart';
+
+class HomeController extends GetxController {
+  //TODO: Implement HomeController
+
+  final count = 0.obs;
+  @override
+  // ignore: unnecessary_overrides
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  // ignore: unnecessary_overrides
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {}
+  void increment() => count.value++;
+}
---
+++ diary_ui/lib/app/modules/home/views/home_view.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/home_controller.dart';
+
+class HomeView extends GetView<HomeController> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HomeView'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () => Get.snackbar(
+                'title',
+                'message',
+              ),
+              child: Text('Show snackbar'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
