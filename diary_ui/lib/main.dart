da1c239f0213526ec6aa335d37bddc47bec1b2a5
Md. Asaduzzaman Rony <ronyku1829@gmail.com>
2021-07-27 18:07:58 +0600

    Commentd sqflite in Main and DatabaseProvider file

--- diary_ui/lib/main.dart
+++ diary_ui/lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-import 'package:sqflite/sqflite.dart';
-import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+// import 'package:sqflite/sqflite.dart';
+// import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() {
-  if (GetPlatform.isWindows || GetPlatform.isLinux) {
-    sqfliteFfiInit();
-    databaseFactory = databaseFactoryFfi;
-  }
+  // if (GetPlatform.isWindows || GetPlatform.isLinux) {
+  //   sqfliteFfiInit();
+  //   databaseFactory = databaseFactoryFfi;
+  // }
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
